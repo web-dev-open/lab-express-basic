5ecabef7d34cbf74d49bc7e7d8c0d662847323aa
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-02-15 16:18:10 +0300

    Jayson Internship-Solved lab

--- app.js
+++ app.js
@@ -17,5 +17,6 @@ app.use((request,response, next)=>{
 app.get('/home',(request,response,next)=>response.sendFile(__dirname + '/views/home-page.html'));
 app.get('/about',(request,response,next)=>response.sendFile(__dirname + '/views/about-page.html'));
 app.get('/works',(request,response,next)=>response.sendFile(__dirname + '/views/works-page.html'));
+app.get('/gallery',(request,response,next)=>response.sendFile(__dirname + '/views/gallery-page.html'));
 //starting server
 app.listen(3000, ()=> console.log("Application listening on port 3000!"))
